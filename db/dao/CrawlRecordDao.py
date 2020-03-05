64460f3bbadb1232bd26d9d1308fb17f11d595b2
Waiting <waitinghc@foxmail.com>
2020-03-05 14:46:50 +0800

    优化查询最后一条记录

--- db/dao/CrawlRecordDao.py
+++ db/dao/CrawlRecordDao.py
@@ -6,6 +6,7 @@ def insert(position, page, name):
     db = DBHelper.Connector().get_connection()
     cursor = db.cursor()
     sql = "insert into crawl_record(position, page,name) VALUES ('{}','{}','{}')".format(position, page, name)
+
     try:
         cursor.execute(sql)
         db.commit()
@@ -18,10 +19,12 @@ def insert(position, page, name):
 def query():
     db = DBHelper.Connector().get_connection()
     cursor = db.cursor()
-    sql = "select position,page,name, max(crawl_id) from crawl_record group by crawl_id"
+    # 查询最后一条记录
+    sql = "select * from crawl_record order by crawl_id desc limit 1"
     try:
         cursor.execute(sql)
         result = cursor.fetchone()
+        print(result)
         if result is not None and len(result) > 0:
             return Entity.CrawlRecord(result[0], result[1], result[2])
     except Exception as ex:
